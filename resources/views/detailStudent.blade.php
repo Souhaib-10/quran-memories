9a9c2f4b986b5e8b96b067e99569de0dc73758cc
souhaib-10 <souhaibb1998@gmail.com>
2025-05-28 18:08:54 +0200

    fix problem alert.js

--- resources/views/detailStudent.blade.php
+++ resources/views/detailStudent.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <script src="{{asset('js/alert.js')}}" defer></script>
+    {{-- <script src="{{asset('js/alert.js')}}" defer></script> --}}
     <title>تفاصيل الطالب</title>
 </head>
 <body>
